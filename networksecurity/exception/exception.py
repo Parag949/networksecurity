dda900d42359dc03eba219209f21a2585d008dc8
Parag <paraggupta1976@gmail.com>
2025-12-03 12:13:48 +0530

    data ingestion component completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -9,5 +9,5 @@ class NetworkSecurityException(Exception):
         self.filename=exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return f"Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(self.filename,self.lineno,str(self.error_message))
+        return f"Error occurred in python script name [{self.filename}] line number [{self.lineno}] error message [{str(self.error_message)}]"
   
